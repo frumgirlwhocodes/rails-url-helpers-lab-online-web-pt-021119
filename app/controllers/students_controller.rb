dadf115da8f181bae9ed57afaf4e76c0d4476148
lieby weintraub <liebahinda@gmail.com>
2019-06-25 23:41:06 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,7 +8,16 @@ class StudentsController < ApplicationController
   def show
   end
   def activate 
-    
+    set_student
+    if @student.active
+      @student.active = false
+    else
+      @student.active = true
+    end
+    @student.save
+
+    redirect_to student_path(@student)
+  end
     
   end 
 
